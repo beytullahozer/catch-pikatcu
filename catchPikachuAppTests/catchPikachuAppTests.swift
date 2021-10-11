18d0364c1fe4e12a1dd75516ed6aae27627f3fbe
beytullahozer <beytullahhozer@gmail.com>
2021-10-11 15:45:07 +0300

    catchPikatcuApp

---
+++ catchPikachuAppTests/catchPikachuAppTests.swift
@@ -0,0 +1,33 @@
+//
+//  catchPikachuAppTests.swift
+//  catchPikachuAppTests
+//
+//  Created by Beytullah Özer on 11.10.2021.
+//
+
+import XCTest
+@testable import catchPikachuApp
+
+class catchPikachuAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
